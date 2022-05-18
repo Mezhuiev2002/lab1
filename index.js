f4913aa5e5a5577d87b985eac544122d15fddcef
Mezhuiev2002 <maxomka77@gmail.com>
2022-05-18 14:31:47 +0300

    lab7

--- index.js
+++ index.js
@@ -1,67 +1,31 @@
-const titl = document.getElementById('title');
 
-titl.onclick = function() {
-    let randomColor = '#'+ Math.floor(Math.random() * 19777215).toString(16);
-    let randColor = '#'+ Math.floor(Math.random() * 19777215).toString(16);
-    titl.style.backgroundColor = randColor;
-    titl.style.color = randomColor;
+
+
+document.querySelector("#download").onclick = function(){
+    for (let i = 0; i < 5; i++) {
+        fetch ('https://randomuser.me/api')            
+            .then((res) => res.json())
+            .then((data) => create(data));
+    }
+  }
+
+function create({results}) {
+    const element = document.querySelector('.table')
+    element.innerHTML += `
+        <div class="column">
+            ${getUser(results)}
+        </div>
+    `
 }
 
-const place = document.querySelector('.place');
-
-place.onclick = function() {
-    let randomColor = '#'+ Math.floor(Math.random() * 19777215).toString(16);
-    let randColor = '#'+ Math.floor(Math.random() * 19777215).toString(16);
-    place.style.backgroundColor = randColor;
-    place.style.color = randomColor;
-}
-
-const image = document.querySelector('#city');
-document.querySelector('.add').addEventListener('click', () => add());
-document.querySelector('.remove').addEventListener('click', () => remove());
-document.querySelector('.increase').addEventListener('click', () => increase());
-document.querySelector('.decrease').addEventListener('click', () => decrease());
-
-function increase() {
-    if(image.classList.contains('small-size')) {
-        image.classList.remove('small-size');
-        image.classList.add('default-size');
-    }
-    else if(image.classList.contains('default-size')) {
-        image.classList.remove('default-size');
-        image.classList.add('big-size');
-    }
-    else {
-        alert("You cannot increase the image anymore!");
-        return
-    }
-}
-
-function decrease() {
-    if(image.classList.contains('big-size')) {
-        image.classList.remove('big-size');
-        image.classList.add('default-size');
-    }
-    else if(image.classList.contains('default-size')) {
-        image.classList.remove('default-size');
-        image.classList.add('small-size');
-    }
-    else {
-        alert("You cannot decrease the image anymore!");
-        return
-    }
-}
-
-function remove() {
-    if(image.classList.contains('show')) {
-        image.classList.remove('show');
-        image.classList.add('delete');
-    }
-}
-
-function add() {
-    if(image.classList.contains('delete')) {
-        image.classList.remove('delete');
-        image.classList.add('show');
-    }
+function getUser(results) {
+    return results.map(el => `
+        <div class="card">
+        <img src="${el.picture.large}" width="200px" height="200px">
+        <p>Location: ${el.location.street.name}, ${el.location.street.number}</p>
+        <p>City: ${el.location.city}, ${el.location.country}</p>
+        <p>E-mail: ${el.email}</p>
+        <p>Coordinates: ${el.location.coordinates.latitude}, ${el.location.coordinates.longitude}</p>
+        </div>
+    `)
 }
